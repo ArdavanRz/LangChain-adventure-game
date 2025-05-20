59f9597eea687d80c74da86f167f6b748b18956e
Ardavan Roozkhosh <93216130+ArdavanRz@users.noreply.github.com>
2025-05-20 23:48:29 +0200

    added cassandra imports and LLM configuration
    added the code for client authorization and configurating OpenAI as the LLM

--- myadv_safe.py
+++ myadv_safe.py
@@ -1,16 +1,44 @@
+from langchain.memory import CassandraChatMessageHistory, ConversationBufferMemory
+from cassandra.cluster import Cluster
+from cassandra.auth import PlainTextAuthProvider
+from langchain.llms import OpenAI
+from langchain import LLMChain, PromptTemplate
 from astrapy import DataAPIClient
+import json
+
+
 
 # Initialize the client
-client = DataAPIClient("your key")
+client = DataAPIClient("")
 db = client.get_database_by_api_endpoint(
 
 
-  "https://ea6839f9-57d1-4042-b8ad-207fdee70842-us-east1.apps.astra.datastax.com"
+  ""
 )
 
 print(f"Connected to Astra DB: {db.list_collection_names()}")
 
+cloud_config= {
+  'secure_connect_bundle': 'secure-connect-choose-your-own-adventure.zip'
+}
+
+with open("choose_your_own_adventure-token.json") as f:
+    secrets = json.load(f)
+
 ##########################
+CLIENT_ID = secrets["clientId"]
+CLIENT_SECRET = secrets["secret"]
 ASTRA_DB_KEYSPACE = ""
 OPENAI_API_KEY = ""
 
+auth_provider = PlainTextAuthProvider(CLIENT_ID, CLIENT_SECRET)
+cluster = Cluster(cloud=cloud_config, auth_provider=auth_provider)
+session = cluster.connect()
+
+
+message_history = CassandraChatMessageHistory(session_id = "anything", session = "session", keyspace = ASTRA_DB_KEYSPACE, ttl_seconds = 3600)
+message_history.clear()
+
+cass_buff_mem = ConversationBufferMemory(memory_key = "chat_history", chat_memory = message_history)
+
+llm = OpenAI(OPENAI_API_KEY=OPENAI_API_KEY)
